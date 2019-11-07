d8a99c1977fa78eaf44064dfb2dde1b1471a5eea
BillMark98 <hupanweibill@gmail.com>
2019-11-07 22:02:46 +0100

    almost done

--- report/readLine.sh
+++ report/readLine.sh
@@ -27,16 +27,21 @@ writeFile='./dataM.txt'
 # do
 #     echo "$word"
 # done
-IFS=' '
+IFS=',|\n|\t'
 while read line; do
     for word in $line; do
     	echo "word has length ${#word}"
     	len=${#word}
     	if [ "$len" -gt 1 ]
     	then
-    		echo " ${word:0:1} "
+            if [ "$len" -gt 2 ] 
+            then
+                echo -n "${word: -1} &" >> "$writeFile"
+            else
+    		      echo " ${word:0:1} \\\\ " >> "$writeFile"
+            fi
     	else 
-    		echo -n	 " $word &"
+    		echo -n	 " $word &" >> "$writeFile"
     	fi
         
     done
